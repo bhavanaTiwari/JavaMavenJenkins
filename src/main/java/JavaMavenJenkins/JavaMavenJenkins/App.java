9d4c9f3650a10b220b9026a7aeaf57c5d8383f47
bhavana tiwari <bhavana.srh@gmail.com>
2018-06-18 15:32:30 +0200

    updated

---
+++ src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
@@ -0,0 +1,13 @@
+package JavaMavenJenkins.JavaMavenJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! jen" );
+    }
+}
