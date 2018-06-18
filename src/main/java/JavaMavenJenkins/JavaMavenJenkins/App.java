f7454e499e8f2224e45d472241d5a2e95831a72d
bhavana tiwari <bhavana.srh@gmail.com>
2018-06-18 15:40:22 +0200

    jekins testing

--- src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
+++ src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
@@ -6,7 +6,7 @@ package JavaMavenJenkins.JavaMavenJenkins;
  */
 public class App 
 {
-    public static void main( String[] args )
+    
     {
         System.out.println( "Hello World! jenkins" )
     }
