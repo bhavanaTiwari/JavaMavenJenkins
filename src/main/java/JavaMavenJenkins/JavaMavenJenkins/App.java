5bab7d7a29abe1fb13fc576addf4e499f409d927
bhavana tiwari <bhavana.srh@gmail.com>
2018-06-18 15:46:07 +0200

    app

--- src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
+++ src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
@@ -9,7 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        SCannner sc = input.n
+        SCannner sc = input.n;
     	System.out.println( "Hello World! jenkins" );
         
     }
