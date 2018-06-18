f3c907ce7457c060fbc75311168526fc5d351559
bhavana tiwari <bhavana.srh@gmail.com>
2018-06-18 15:34:23 +0200

    app

--- src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
+++ src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! jen" );
+        System.out.println( "Hello World! jenkins" );
     }
 }
