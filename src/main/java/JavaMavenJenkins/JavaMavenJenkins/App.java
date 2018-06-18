50c04c380bf3c1b4923242afd7f4fc7536eb9dee
bhavana tiwari <bhavana.srh@gmail.com>
2018-06-18 15:56:50 +0200

    error

--- src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
+++ src/main/java/JavaMavenJenkins/JavaMavenJenkins/App.java
@@ -9,8 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        SCannner sc = input.n;
-    	System.out.println( "Hello World! jenkins" );
+        hfgbcv
         
     }
 }
